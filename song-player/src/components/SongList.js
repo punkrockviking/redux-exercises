9fe8e1c19a3aed92d3a00b914f27718a5835b044
punkrockviking <danjeccles@gmail.com>
2022-08-22 17:54:37 -0600

    feat: mapStateToProps for SongList component. connect to redux state

--- song-player/src/components/SongList.js
+++ song-player/src/components/SongList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class SongList extends React.Component {
   render() {
@@ -6,4 +7,10 @@ class SongList extends React.Component {
   }
 }
 
-export default SongList;
+const mapStateToProps = (state) => {
+  // take state object from Redux store then make that data show up as props inside our component
+  return { songs: state.songs };
+};
+
+export default connect(mapStateToProps)(SongList); // second set of parenthesis invokes the function returned by the first function
+// connect(invoke function that returns a function)(invoke second function)
